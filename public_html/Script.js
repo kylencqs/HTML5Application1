b4d85242a8e092bdee61d85f04efc0316454e825
kylen <kylen@KYLEN_04.cqs.co.za>
2015-04-08 12:37:11 +0200

    First commit test

---
+++ public_html/Script.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Index.html'>Home</a></li>" +
+"<li><a href='Customers.html'>Data</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"</ul>";
